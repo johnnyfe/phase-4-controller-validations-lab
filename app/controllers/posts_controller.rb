7d36b6a081752ee798fb362c4af50d70f48ebdd3
johnny <johnny1194@hotmail.com>
2021-08-30 00:05:15 -0400

    Completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def show
     post = Post.find(params[:id])
     
@@ -8,10 +7,10 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
